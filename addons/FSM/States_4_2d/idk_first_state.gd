907222c4fd67ecb7d4e064154069ea5cf6816bdc
Hadr <casilyas1@gmail.com>
2024-10-07 19:16:31 +0300

    hui

---
+++ addons/FSM/States_4_2d/idk_first_state.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+enum States {IDLE, RUNNING, JUMPING, FALLING}
+
+#current state
+var state: States = States.IDLE
+
+
+func Enter():
+	pass
+	
+func Exit():
+	pass
+	
+func Update():
+	pass
+	
+func Physics_update():
+	pass
